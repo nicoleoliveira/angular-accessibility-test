91a161ab2b5878eeba86255803e61135b85e02e7
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2019-12-02 09:19:17 -0300

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import * as axe from 'axe-core';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -28,4 +30,34 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-test-accessibility!');
   });
+
+  it('should be no accessibility issues', done => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+
+    axe.run(compiled, {}, (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      if (results.violations.length > 0) {
+        console.log(JSON.stringify(results.violations, null, 4));
+      }
+
+      expect(results.violations.length).toBe(0);
+      done();
+    });
+  });
+
+  // it('should be no accessibility issues', done => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+
+  //   axe.a11yCheck(compiled, {}, result => {
+  //     expect(result.violations.length).toBe(0);
+  //     done();
+  //   });
+  // });
 });
